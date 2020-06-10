9b8add3f36192bd57f0391d210422b67f01375e9
Mishari Aliesa <aliesa@usc.edu>
2020-06-09 18:36:27 -0700

    Refactor out OffPolicyRLAlgorithm

--- src/garage/np/__init__.py
+++ src/garage/np/__init__.py
@@ -1,4 +1,6 @@
 """Reinforcement Learning Algorithms which use NumPy as a numerical backend."""
+from garage.np._functions import obtain_evaluation_samples
 from garage.np._functions import paths_to_tensors
+from garage.np._functions import process_paths
 
-__all__ = ['paths_to_tensors']
+__all__ = ['obtain_evaluation_samples', 'paths_to_tensors', 'process_paths']
